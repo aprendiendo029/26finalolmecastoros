f41333469528a54b5e2af89631e94de11d5b2eff
WFX1o4 <wfx1o4@Mac-mini-de-WFX1o4.local>
2026-09-06 17:37:33 -0600

    Configuracion base para GitHub Pages en vite.config

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // Reemplaza 'tu-repositorio' por el nombre EXACTO de tu repositorio en GitHub
+  base: '/26finalolmecastoros/', 
+  build: {
+    outDir: 'dist',
+  }
+})
